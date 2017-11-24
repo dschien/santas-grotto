03cd99e31d8ff4b3949e4f7f96146121f25dae57
dschien <dschien@gmail.com>
2017-11-24 01:59:10 +0000

    renamed to santas grotto

--- src/main/java/ac/uk/bristol/cs/santa/grotto/SantasGrottoApp.java
+++ src/main/java/ac/uk/bristol/cs/santa/grotto/SantasGrottoApp.java
@@ -1,13 +1,14 @@
-package ac.uk.bristol.cs.spe8.demo;
+package ac.uk.bristol.cs.santa.grotto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SantasGrottoApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(SantasGrottoApp.class, args);
     }
 
 }
