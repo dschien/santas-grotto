6184d917139248421c973bfbdc25a876abb68e2e
dschien <dschien@gmail.com>
2017-11-24 02:00:00 +0000

    renamed application.properties to application.yml add property value source in user home for secrets

--- src/main/java/ac/uk/bristol/cs/santa/grotto/SantasGrottoApp.java
+++ src/main/java/ac/uk/bristol/cs/santa/grotto/SantasGrottoApp.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("file:${user.home}/.secret.properties")
 @SpringBootApplication
 public class SantasGrottoApp {
 
